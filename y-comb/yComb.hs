ad537f6526740808dad36cd4e8bc5bd62478ed03
Turab Jafri <trajafri@gmail.com>
2021-02-28 16:33:14 -0600

    add yComb solution in Haskell

---
+++ y-comb/yComb.hs
@@ -0,0 +1,11 @@
+newtype Rec a = Rec {unwrap :: (Rec a) -> a}
+
+selfApp :: (a -> a) -> a
+selfApp f =  let selfAppInternal rec = f ((unwrap rec) rec) in
+  selfAppInternal (Rec selfAppInternal)
+
+factorial :: Integer -> Integer
+factorial = selfApp(\rec n -> if n == 0 then 1 else n * (rec (n - 1)))
+
+maxNat :: Integer -> Integer -> Integer
+maxNat = selfApp(\rec m n -> if m == 0 then n else if n == 0 then m else succ (rec (m - 1) (n - 1)))
